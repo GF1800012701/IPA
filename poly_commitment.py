18888944b9815516a57ad2ff43a318ef551e3155
GF1800012701 <1800012701@pku.edu.cn>
2023-06-22 16:33:06 +0800

    over

--- poly_commitment.py
+++ poly_commitment.py
@@ -3,9 +3,33 @@
 from prime_group import PrimeGroup
 
 class Verifier:
-    def __init__(self, group):
+    def __init__(self, group, g, c, y, v):
         self.group = group
+        self.g = g
+        self.c = c
+        self.y = y
+        self.v = v
 
+    def challenge(self, cv_tuple):
+        a = self.group.generate_random_vector(1)[0]
+        self.c = self.group.update_num(self.c, a, cv_tuple[0], cv_tuple[1])
+        self.v = self.group.update_num(self.v, a, cv_tuple[2], cv_tuple[3])
+
+        self.g = self.group.update_list(self.g, a)
+        self.y = self.group.update_list(self.y, a)
+
+        self.print_status("challenge")
+        return (a, self.c, self.v, self.g, self.y)
+
+    def final_verify(self, final_tuple):
+        u = final_tuple[0]
+        if self.group.multiply(u, self.g[0]) == c and self.group.multiply(u, self.y[0]) == v :
+            print("True!")
+        else:
+            print("False!")
+
+    def print_status(self, str):
+        print(str, self.g, self.c, self.y, self.v)
     
 
 class Prover:
@@ -13,14 +37,76 @@ class Prover:
         self.group = group
         self.u = u
 
+    def init_g(self, g):
+        self.g = g
+        self.c = self.group.inner_product(u, g)
+        return self.c
 
+    def init_y(self, y):
+        self.y = y
+        self.v = self.group.inner_product(u, y)
+        return self.v
+    
+    def one_prove(self):
+        length = len(self.u)
+        print("length", length)
+        
+        if length == 1:
+            return (self.u)
+        else:
+            ul = self.u[ : length//2]
+            ur = self.u[length//2 : ]
+            gl = self.g[ : length//2]
+            gr = self.g[length//2 : ]
+            yl = self.y[ : length//2]
+            yr = self.y[length//2 : ]
+
+            cl = self.group.inner_product(ul, gr)
+            cr = self.group.inner_product(ur, gl)
+            vl = self.group.inner_product(ul, yr)
+            vr = self.group.inner_product(ur, yl)
+
+            self.print_status("one_prove")
+            return (cl, cr, vl, vr)
+
+    def update(self, update_tuple):
+        (a, c, v, g, y) = update_tuple
+        self.c = c
+        self.v = v
+        self.g = g
+        self.y = y
+        self.u = self.group.update_list(self.u, a)
+        self.print_status("update")
+        return
+
+    def print_status(self, str):
+        print(str, self.u, self.g, self.c, self.y, self.v)
 
 if __name__ == "__main__":
     group = PrimeGroup(101)
     u = group.generate_random_vector(8)
-
+    prover = Prover(group, u)
 
     g = group.generate_random_vector(8)
+    c = prover.init_g(g)
+
     y = group.generate_random_vector(8)
+    v = prover.init_y(y)
+
+    verifier = Verifier(group, g, c, y, v)
+
+    # start
+    while True:
+        cv_tuple = prover.one_prove()
+        print("cv_tuple : ", cv_tuple)
+        if len(cv_tuple) == 1:
+            
+            verifier.final_verify(cv_tuple)
+            break
+        update_tuple = verifier.challenge(cv_tuple)
+        print("update_tuple : ", update_tuple)
+        prover.update(update_tuple)
+
+
 
 
--- prime_group.py
+++ prime_group.py
@@ -1,3 +1,5 @@
+import random
+
 class PrimeGroup:
     def __init__(self, order):
         self.p = order  # 素数p作为群的模数
@@ -18,8 +20,29 @@ class PrimeGroup:
     def inverse(self, a):
         return pow(a, self.p - 2, self.p)  # 使用费马小定理计算模逆元
     
-    def generate_random_vector(self, len):
-        pass
+    def generate_random_vector(self, l):
+        return [random.randint(2, self.p-1) for _ in range(l)]
+
+    def inner_product(self, list1, list2):
+        sum = 0
+        if(len(list1) != len(list2)):
+            print("length error!", list1, list2)
+        for i  in range(len(list1)):
+            sum = self.add(sum, self.multiply(list1[i], list2[i]))
+        return sum
+
+    def update_num(self, n, a, left, right):
+        result = n
+        result = self.add(self.multiply(self.multiply(left, a), a), result)
+        result = self.add(self.divide(self.divide(right, a), a), result)
+        return result
+
+    def update_list(self, list, a):
+        mid_index = len(list) // 2
+        left_list = [self.multiply(item, a) for item in list[:mid_index]]
+        right_list = [self.divide(item, a) for item in list[mid_index:]]
+        return [self.add(item1, item2) for item1, item2 in zip(left_list, right_list)]
+
 
 if __name__ == "__main__":
     # 创建一个 101 阶素数群的实例
@@ -40,3 +63,5 @@ if __name__ == "__main__":
     # 测试除法
     result = prime_Group.divide(80, 4)
     print("80 / 4 =", result)
+
+#如何在python中生成一个大于1 小于p的整数随机数？如何生成这样的随机数的list，需要指定长度l？
